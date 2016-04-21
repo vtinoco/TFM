bdb0f2e0f6848a7cc270ea4cf63fe84ecc14bc83
Víctor Tinoco <victor.tinoco.86@gmail.com>
2016-04-21 18:15:18 +0200

    Update karaokeSearch.py

--- karaokeSearch.py
+++ karaokeSearch.py
@@ -12,8 +12,8 @@
 ##     You should have received a copy of the GNU General Public License        ## 
 ##     along with this program.  If not, see <http://www.gnu.org/licenses/>.    ## 
 ##                                                                              ##
-##    Copyleft 2016 Victor Tinoco Horrillo                                      ##
-##		correo: victor.tinoco.86@gmail.com  												              ##
+##     Copyleft 2016 Victor Tinoco Horrillo                                     ##
+##     correo: victor.tinoco.86@gmail.com        				##
 ##                                                                              ##
 ##################################################################################
 
